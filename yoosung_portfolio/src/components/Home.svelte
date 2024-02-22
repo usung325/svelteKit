62b7a184e0c0489612fa110cc9e9d756c946d80a
usung325 <usung325@gmail.com>
2024-02-22 11:12:59 -0500

    Set up components

---
+++ yoosung_portfolio/src/components/Home.svelte
@@ -0,0 +1,85 @@
+<script>
+
+</script>
+
+<body class="overscroll-behavior-x: auto;">
+    <div id = 'main'>
+        <div class="md:flex md:flex-row md:justify-center">
+            <div class=" md:min-w-[512px] md:w-full md:max-w-xl">
+                <div class="flex flex-col justify-start max-w-xl px-8 py-24 ">
+                    <div class="flex flex-col gap-8 pb-8">
+                        <h1 class="inline-block mx-auto">This is my website</h1>
+                        
+                        <p class="inline-block text-gray-400 "> These are projects that I've current worked on for the past couple years.</p>
+                        
+
+
+                        <!-- this is content -->
+                        <div class="flex flex-col gap-6">
+                            <div class="contentBlock">
+                                <div class="flex flex-col gap-2">
+                                    <div class="flex flex-row justify-center bg-gray-50 rounded-sm md:-mx-24 p-1">
+                                        <img alt='neek thumbnail' width="2400" height="1260" class="object-contain rounded-sm" src="/g.png">
+
+                                    </div>
+                                    <div class="flex flex-col px-2">
+                                        <div class="flex flex-row justify-between md:-mx-24">
+                                            <p> NEEK Branding </p>
+                                            <p class="text-gray-400"> Illustration</p>
+                                        </div>
+                                        <p class="text-gray-400 md:-mx-24">2023</p>
+
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div class="contentBlock">
+                                <div class="flex flex-col gap-2">
+                                    <div class="flex flex-row justify-center bg-gray-50 rounded-sm md:-mx-24 p-1">
+                                        <img alt='neek thumbnail' width="2400" height="1260" class="object-contain rounded-sm" src="/g.png">
+
+                                    </div>
+                                    <div class="flex flex-col px-2">
+                                        <div class="flex flex-row justify-between md:-mx-24">
+                                            <p> NEEK Branding </p>
+                                            <p class="text-gray-400"> Illustration</p>
+                                        </div>
+                                        <p class="text-gray-400 md:-mx-24">2023</p>
+
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div class="contentBlock">
+                                <div class="flex flex-col gap-2">
+                                    <div class="flex flex-row justify-center bg-gray-50 rounded-sm md:-mx-24 p-1">
+                                        <img alt='neek thumbnail' width="2400" height="1260" class="object-contain rounded-sm" src="/g.png">
+
+                                    </div>
+                                    <div class="flex flex-col px-2">
+                                        <div class="flex flex-row justify-between md:-mx-24">
+                                            <p> NEEK Branding </p>
+                                            <p class="text-gray-400"> Illustration</p>
+                                        </div>
+                                        <p class="text-gray-400 md:-mx-24">2023</p>
+
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+
+    
+</body>
--- yoosung_portfolio/src/routes/+page.svelte
+++ yoosung_portfolio/src/routes/+page.svelte
@@ -1,11 +1,14 @@
 <script>
     import Footer from "../components/Footer.svelte";
     import Header from "../components/Header.svelte";
+    import Home from "../components/Home.svelte";
 
 
 </script>
 
 <Header />
+
+<Home  />
 
 <h1>Welcome to SvelteKit</h1>
 <br>
