0c59bf13a250afc8f6e9a4b8be9147a60e7d1d8a
usung325 <usung325@gmail.com>
2024-03-18 14:57:09 -0400

    test

--- yoosung_portfolio/tailwind.config.js
+++ yoosung_portfolio/tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
+      future: {
+        hoverOnlyWhenSupported: true,
+     },
       screens: {
         'hover-hover': {'raw': '(hover: hover)'},
       },
