bc0e359b60146abbc756048e10484de49d1234db
usung325 <usung325@gmail.com>
2024-03-19 01:21:05 -0400

    externalizedDeps plugin

--- yoosung_portfolio/vite.config.js
+++ yoosung_portfolio/vite.config.js
@@ -1,8 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import externalizeDeps from 'vite-plugin-externalize-deps';
+
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		externalizeDeps() // Use the plugin
+	 ],
 	build: {
 	   rollupOptions: {
 		 external: ['matter-js'] // Add other external dependencies here
