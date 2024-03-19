1313d59b204903cc1a72a82ca40c60691ff11c8a
usung325 <usung325@gmail.com>
2024-03-19 01:22:27 -0400

    edit

--- yoosung_portfolio/vite.config.js
+++ yoosung_portfolio/vite.config.js
@@ -1,12 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import externalizeDeps from 'vite-plugin-externalize-deps';
+
 
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
-		externalizeDeps() // Use the plugin
 	 ],
 	build: {
 	   rollupOptions: {
