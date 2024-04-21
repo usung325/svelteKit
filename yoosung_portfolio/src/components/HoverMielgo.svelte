8a81a9c619e7d91a144e3d6e0bc799228745d451
usung325 <usung325@gmail.com>
2024-04-21 02:32:09 -0400

    40%

--- yoosung_portfolio/src/components/HoverMielgo.svelte
+++ yoosung_portfolio/src/components/HoverMielgo.svelte
@@ -12,20 +12,22 @@
 
         //////////////////////////////////////////////////////////////////////////////////
     }
+
+    // let width = window.innerWidth;
 </script>
 
-<main class="flex flex-row h-full px-5 items-center">
+<main class="flex flex-row h-full px-2 items-center">
     <div class="space-y-10">
-        <img in:fade={{delay: 100}}  alt="profile" src="/mielgo2.gif" class="w-[45%]" />
-        <img in:fade={{delay: 400}}  alt="profile" src="/mielgo2.gif" class="w-[45%]" />
-        <img in:fade={{delay: 600}}  alt="profile" src="/mielgo2.gif" class="w-[45%]" />
+        <img in:fade={{delay: 100}}  alt="profile" src="/mielgo2.gif" class="w-[40%]" />
+        <img in:fade={{delay: 400}}  alt="profile" src="/mielgo2.gif" class="w-[40%]" />
+        <img in:fade={{delay: 600}}  alt="profile" src="/mielgo2.gif" class="w-[40%]" />
     </div>
 
     <div class="space-y-10">
         <!-- <div class="space-y-2 flex flex-col "> -->
-            <img in:fade={{delay: 300}}  alt="profile" src="/mielgo2.gif" class="w-[45%] ml-auto" />
-            <img in:fade={{delay: 500}}  alt="profile" src="/mielgo2.gif" class="w-[45%] ml-auto" />
-            <img in:fade={{delay: 200}}  alt="profile" src="/mielgo2.gif" class="w-[45%] ml-auto" />
+            <img in:fade={{delay: 300}}  alt="profile" src="/mielgo2.gif" class="w-[40%] ml-auto" />
+            <img in:fade={{delay: 500}}  alt="profile" src="/mielgo2.gif" class="w-[40%] ml-auto" />
+            <img in:fade={{delay: 200}}  alt="profile" src="/mielgo2.gif" class="w-[40%] ml-auto" />
         <!-- </div> -->
     </div>
 </main>
