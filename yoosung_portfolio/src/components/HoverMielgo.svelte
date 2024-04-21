286cd7681e8fa1d4d25703b4abed9c4a5fc2ca0e
usung325 <usung325@gmail.com>
2024-04-21 02:47:55 -0400

    revert test

--- yoosung_portfolio/src/components/HoverMielgo.svelte
+++ yoosung_portfolio/src/components/HoverMielgo.svelte
@@ -13,23 +13,23 @@
         //////////////////////////////////////////////////////////////////////////////////
     }
     let width = window.innerWidth;
-    let widthW = (((window.innerWidth - 700) / 2) - 10);
+    let widthW = ((window.innerWidth - 700) / 2) - 10;
     let ratio = Math.floor((widthW / width) * 100)
     console.log(ratio);
 </script>
 
 <main class="flex flex-row h-full px-2 items-center">
     <div class="space-y-10">
-        <img in:fade={{delay: 100}}  alt="profile" src="/mielgo2.gif" class="w-[{ratio}%]" />
-        <img in:fade={{delay: 400}}  alt="profile" src="/mielgo2.gif" class="w-[{ratio}%]" />
-        <img in:fade={{delay: 600}}  alt="profile" src="/mielgo2.gif" class="w-[{ratio}%]" />
+        <img in:fade={{delay: 100}}  alt="profile" src="/mielgo2.gif" class="w-[35%]" />
+        <img in:fade={{delay: 400}}  alt="profile" src="/mielgo2.gif" class="w-[35%]" />
+        <img in:fade={{delay: 600}}  alt="profile" src="/mielgo2.gif" class="w-[35%]" />
     </div>
 
     <div class="space-y-10">
         <!-- <div class="space-y-2 flex flex-col "> -->
-            <img in:fade={{delay: 300}}  alt="profile" src="/mielgo2.gif" class="w-[{ratio}%] ml-auto" />
-            <img in:fade={{delay: 500}}  alt="profile" src="/mielgo2.gif" class="w-[{ratio}%] ml-auto" />
-            <img in:fade={{delay: 200}}  alt="profile" src="/mielgo2.gif" class="w-[{ratio}%] ml-auto" />
+            <img in:fade={{delay: 300}}  alt="profile" src="/mielgo2.gif" class="w-[35%] ml-auto" />
+            <img in:fade={{delay: 500}}  alt="profile" src="/mielgo2.gif" class="w-[35%] ml-auto" />
+            <img in:fade={{delay: 200}}  alt="profile" src="/mielgo2.gif" class="w-[35%] ml-auto" />
         <!-- </div> -->
     </div>
 </main>
