0bfcb2cd2e9e3967ff8e2c19e4bf82e8242618ba
usung325 <usung325@gmail.com>
2024-04-20 22:37:30 -0400

    comments

--- yoosung_portfolio/src/components/HoverStickQs.svelte
+++ yoosung_portfolio/src/components/HoverStickQs.svelte
@@ -1,11 +1,15 @@
 <script>
     // import { showOverlay } from './Home.svelte';
+
+    ///////////////// ATTEMPT AT PRELOADING IMAGE BEFORE HOVER ///////////////////////
     export const prerender = true;
 
     /** @type {import('./$types').PageLoad} */
     export async function load({ fetch }) {
         const res = await fetch("/my-server-route.json");
         return await res.json();
+
+    //////////////////////////////////////////////////////////////////////////////////
     }
 </script>
 
