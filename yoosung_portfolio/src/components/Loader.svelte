6459392c99349b0826842785b378a49919475699
usung325 <usung325@gmail.com>
2024-04-20 22:52:49 -0400

    loading page

--- yoosung_portfolio/src/components/Loader.svelte
+++ yoosung_portfolio/src/components/Loader.svelte
@@ -1,11 +1,13 @@
 <script>
+    let width = window.innerWidth;
+    let height = window.innerHeight;
 
 </script>
 
 
 <main>
-    <div class="flex flex-col content-center items-center mx-auto text-center text-lg">
-        <div>
+    <div class="flex flex-col content-center items-center mx-auto text-center text-lg" >
+        <div class="mt-[50%]">
             <h1 class="animate-bounce"> loading... </h1>
         </div>
     </div>
