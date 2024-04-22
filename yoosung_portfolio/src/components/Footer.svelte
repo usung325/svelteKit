6cd9af70501d52c10635671cca1aab4ebaa84017
usung325 <usung325@gmail.com>
2024-04-22 01:08:58 -0400

    spin the hammer

--- yoosung_portfolio/src/components/Footer.svelte
+++ yoosung_portfolio/src/components/Footer.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <div class="text-center pt-[5em]">
-    <i class="fa-solid fa-hammer text-slate-400"></i>
+    <i class="fa-solid fa-hammer text-slate-400 hover:animate-spin"></i>
     <p class="text-slate-400">made from scratch</p>
     <p class="text-slate-400"> © 2024 | Yoo Sung Lee</p>
 </div>
