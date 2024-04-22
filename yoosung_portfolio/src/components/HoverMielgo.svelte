becd62fe528c69595ba418a5fcbb2fe7f4b45760
usung325 <usung325@gmail.com>
2024-04-21 22:34:57 -0400

    updated hover on Mielgo

--- yoosung_portfolio/src/components/HoverMielgo.svelte
+++ yoosung_portfolio/src/components/HoverMielgo.svelte
@@ -1,4 +1,4 @@
-<script>
+<!-- <script>
     // import { showOverlay } from './Home.svelte';
     import { fade, fly } from "svelte/transition";
 
@@ -26,10 +26,48 @@
     </div>
 
     <div class="space-y-10">
-        <!-- <div class="space-y-2 flex flex-col "> -->
+
             <img in:fade={{delay: 300}}  alt="profile" src="/mielgo2.gif" class="w-[43%] ml-auto" />
             <img in:fade={{delay: 500}}  alt="profile" src="/mielgo2.gif" class="w-[43%] ml-auto" />
             <img in:fade={{delay: 200}}  alt="profile" src="/mielgo2.gif" class="w-[43%] ml-auto" />
+
+    </div>
+</main> -->
+
+
+<script>
+    // import { showOverlay } from './Home.svelte';
+    import { fade, fly } from "svelte/transition";
+
+    ///////////////// ATTEMPT AT PRELOADING IMAGE BEFORE HOVER ///////////////////////
+    export const prerender = true;
+
+    /** @type {import('./$types').PageLoad} */
+    export async function load({ fetch }) {
+        const res = await fetch("/my-server-route.json");
+        return await res.json();
+
+        //////////////////////////////////////////////////////////////////////////////////
+    }
+    let width = window.innerWidth;
+    let widthW = ((window.innerWidth - 700) / 2);
+    // let ratio = Math.floor((widthW / width) * 100)
+    // console.log(ratio);
+</script>
+
+<main class="flex flex-row h-full px-5 items-center  justify-between">
+    <div class="space-y-24">
+        <img in:fade={{delay: 100}}  alt="profile" src="/mielgo/vid1.gif" class="w-[65%]" />
+        <img in:fade={{delay: 400}}  alt="profile" src="/mielgo/vid2.gif" class="w-[65%]" />
+        <img in:fade={{delay: 600}}  alt="profile" src="/mielgo/vid3.gif" class="w-[65%]" />
+    </div>
+
+    <div class="space-y-24 ">
+        <!-- <div class="space-y-2 flex flex-col "> -->
+            <img in:fade={{delay: 300}}  alt="profile" src="/mielgo/vid2.gif" class="w-[65%] ml-auto" />
+            <img in:fade={{delay: 500}}  alt="profile" src="/mielgo/vid3.gif" class="w-[65%] ml-auto" />
+            <img in:fade={{delay: 200}}  alt="profile" src="/mielgo/vid1.gif" class="w-[65%] ml-auto" />
         <!-- </div> -->
     </div>
 </main>
+
