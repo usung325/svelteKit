5d14ec3804af21b28b8408dc90c2d6dd49ffe331
usung325 <usung325@gmail.com>
2024-04-22 00:15:54 -0400

    added copyright

--- yoosung_portfolio/src/components/Footer.svelte
+++ yoosung_portfolio/src/components/Footer.svelte
@@ -4,4 +4,5 @@
 <div class="text-center pt-[5em]">
     <i class="fa-solid fa-hammer text-slate-400"></i>
     <p class="text-slate-400">made from scratch</p>
+    <p class="text-slate-400"> © 2024 | Yoo Sung Lee</p>
 </div>
