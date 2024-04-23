4a7bd9c2b088fbbd9768746609cacc2d794c41ce
usung325 <usung325@gmail.com>
2024-04-23 02:03:31 -0400

    'coded' instead of 'made'

--- yoosung_portfolio/src/components/Footer.svelte
+++ yoosung_portfolio/src/components/Footer.svelte
@@ -3,6 +3,6 @@
 
 <div class="text-center pt-[5em]">
     <i class="fa-solid fa-hammer text-slate-400 hover:animate-spin"></i>
-    <p class="text-slate-400">made from scratch</p>
+    <p class="text-slate-400">coded from scratch</p>
     <!-- <p class="text-slate-400"> © 2024 | Yoo Sung Lee</p> -->
 </div>
